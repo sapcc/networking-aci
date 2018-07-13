6be5b29b6c06b895bd149cb5d4bcb223486b2eeb
Andrew Karpow <andrew.karpow@sap.com>
2018-07-13 18:03:54 +0200

    fixup import

--- networking_aci/db/migration/alembic_migrations/env.py
+++ networking_aci/db/migration/alembic_migrations/env.py
@@ -8,7 +8,7 @@ from sqlalchemy import event
 
 from neutron.db.migration.alembic_migrations import external
 from neutron.db.migration.models import head  # noqa
-from neutron.db import model_base
+from neutron_lib.db import model_base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
