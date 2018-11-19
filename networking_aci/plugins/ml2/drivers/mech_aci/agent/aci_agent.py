1ee7889d9a457e3be35c0ce689209d2f98c92f5b
Andrew Karpow <andrew.karpow@sap.com>
2018-11-19 18:31:56 +0100

    fix race condition resulting in invalid marker

--- networking_aci/plugins/ml2/drivers/mech_aci/agent/aci_agent.py
+++ networking_aci/plugins/ml2/drivers/mech_aci/agent/aci_agent.py
@@ -36,6 +36,7 @@ from neutron.common import topics
 from neutron.i18n import _LE
 from neutron.db import db_base_plugin_v2 as db
 from neutron import context
+from neutron_lib.exceptions import NetworkNotFound
 
 from networking_aci.plugins.ml2.drivers.mech_aci import cobra_manager
 from networking_aci.plugins.ml2.drivers.mech_aci import constants as aci_constants
@@ -272,12 +273,15 @@ class AciNeutronAgent(rpc_api.ACIRpcAPI):
 
                     start = time.time()
 
-                    neutron_networks = self.agent_rpc.get_networks(limit=str(self.sync_batch_size), marker=self.sync_marker)
-
-                    if not neutron_networks:
+                    try:
+                        neutron_networks = self.agent_rpc.get_networks(limit=str(self.sync_batch_size), marker=self.sync_marker)
+                    except NetworkNotFound:
                         self.sync_marker = None
                         continue
 
+                    if len(neutron_networks) == 0:
+                        self.sync_marker = None
+                        continue
 
                     for network in neutron_networks:
                         try:
