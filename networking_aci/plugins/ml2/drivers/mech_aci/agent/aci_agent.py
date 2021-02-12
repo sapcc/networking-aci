ded7c59cf2f3a267a7358d6dcdc310d9fb8e896b
Sebastian Lohff <sebastian.lohff@sap.com>
2021-02-12 11:42:25 +0100

    Disable config reload via SIGHUP
    This feature is not used by us, also we're going to go with immutable
    config maps for deploying the driver.

--- networking_aci/plugins/ml2/drivers/mech_aci/agent/aci_agent.py
+++ networking_aci/plugins/ml2/drivers/mech_aci/agent/aci_agent.py
@@ -147,13 +147,6 @@ class AciNeutronAgent(rpc_api.ACIRpcAPI):
             LOG.info(_LI("Agent caught SIGTERM, quitting daemon loop."))
             self.run_daemon_loop = False
             self.catch_sigterm = False
-        if self.catch_sighup:
-            LOG.info(_LI("Agent caught SIGHUP, resetting."))
-            self.conf.reload_config_files()
-            config.setup_logging()
-            LOG.debug('Full set of CONF:')
-            self.conf.log_opt_values(LOG, logging.DEBUG)
-            self.catch_sighup = False
         return self.run_daemon_loop
 
     def _handle_sigterm(self, signum, frame):
