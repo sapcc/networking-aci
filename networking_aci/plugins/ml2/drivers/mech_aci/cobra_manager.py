sapcc/networking-aci

commit 5fd5931717e96efe9020d8fc0d948af327284984

Add config opts to split up contracts datastructure
The datastructure for address scope provided/consumed contracts is
needlessly complicated. Moving forward we can use consumed_contracts and
provided_contracts instead. For the time being contracts will be parsed
and extend those two lists.

networking_aci/plugins/ml2/drivers/mech_aci/cobra_manager.py
@@ -12,8 +12,6 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-import ast
-
 from cobra.model import fv
 from cobra.model import phys
 from cobra import modelimpl
@@ -255,21 +253,17 @@ class CobraManager(object):
         epg = fv.AEPg(app, network_id)
 
         epg_contracts = []
+        for consumed in scope_config['consumed_contracts']:
+            contract = fv.RsCons(epg, consumed)
+            if delete and last_on_network:
+                contract.delete()
+            epg_contracts.append(contract)
 
-        contract_def = ast.literal_eval(scope_config['contracts'])
-        for contract_type, contracts in contract_def.iteritems():
-            if contract_type == 'consumed':
-                for contract in contracts:
-                    contract = fv.RsCons(epg, contract)
-                    if delete and last_on_network:
-                        contract.delete()
-                    epg_contracts.append(contract)
-            elif contract_type == 'provided':
-                for contract in contracts:
-                    contract = fv.RsProv(epg, contract)
-                    if delete and last_on_network:
-                        contract.delete()
-                    epg_contracts.append(contract)
+        for provided in scope_config['provided_contracts']:
+            contract = fv.RsCons(epg, provided)
+            if delete and last_on_network:
+                contract.delete()
+            epg_contracts.append(contract)
 
         self.apic.commit([vrf] + bd_outs + epg_contracts)
 

networking_aci/plugins/ml2/drivers/mech_aci/config.py
@@ -12,6 +12,8 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+import ast
+
 from oslo_config import cfg
 
 DEFAULT_ROOT_HELPER = ('sudo /usr/local/bin/neutron-rootwrap '
@@ -111,8 +113,12 @@ fixed_binding_opts = [
 address_scope_opts = [
     cfg.ListOpt('l3_outs', required=True,
                 help="List of l3outs for this address scope"),
-    cfg.StrOpt('contracts', required=True,
+    cfg.StrOpt('contracts',
                help="Contract data structure, e.g. {'consumed':['foo'],'provided':['foo']}"),
+    cfg.ListOpt('consumed_contracts',
+                help="Consumed contracts for this scope. Contracts from the contracts option will be added as well."),
+    cfg.ListOpt('provided_contracts',
+                help="Provided contracts for this scope. Contracts from the contracts option will be added as well."),
     cfg.StrOpt('vrf', required=True,
                help="VRF name of this address scope"),
 ]
@@ -192,6 +198,12 @@ class ACIConfig:
 
     def _parse_address_scopes(self):
         self._address_scopes = self._parse_config("address-scope", address_scope_opts, to_dict=True)
+
+        for scope in self._address_scopes:
+            if scope.get('contracts'):
+                contracts = ast.literal_eval(scope.get('contracts'))
+                scope['consumed_contracts'].extend(contracts['consumed'])
+                scope['provided_contracts'].extend(contracts['provided'])
 
     def get_hostgroup_by_host(self, host_id):
         for hostgroup, hostgroup_config in self.hostgroups.items():

commit 4f2284cfaff0b12aaf360922a44bdd540b276109

Fix providing contract bug on external net create
When an external subnet is created in OpenStack the external network it
belongs to is created/updated and we make sure that it has the
appropriate providing/consuming contracts. Due to a refactor the
creation of providing and consuming contracts were reduced to only
create consuming contracts, which should now have been rectified. This
only causes problem with external networks that have been created after
deployment of this commit, but as there is no syncloop ensuring external
network config these networks need to be synced manually.

The bug was introduced in 5fd5931717e96efe9020d8fc0d948af327284984

networking_aci/plugins/ml2/drivers/mech_aci/cobra_manager.py
@@ -363,7 +363,7 @@ class CobraManager(object):
             epg_contracts.append(contract)
 
         for provided in scope_config['provided_contracts']:
-            contract = fv.RsCons(epg, provided)
+            contract = fv.RsProv(epg, provided)
             if delete and last_on_network:
                 contract.delete()
             epg_contracts.append(contract)