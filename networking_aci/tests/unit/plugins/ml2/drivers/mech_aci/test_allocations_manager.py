1d653372ce43751d3faa40d884b5b33d97fbe5ff
Sebastian Lohff <sebastian.lohff@sap.com>
2024-03-14 12:03:08 +0100

    Fix and improve allocation cleaning sync
    This is a rewrite of the sync allocations algorithm, which should be
    about 80% faster (due to different way to query the DB), hopefully
    better to understand and should actually clean allocations up (as before
    the deletion happened outside of a transaction and was never actually
    committed).
    
    I've started out to understand the original sync algorithm and why it is
    so slow and arrived with a rewrite of it in the way. The rewrite ended
    up being 5% faster (which is basically nothing in this context), but
    hopefully it is better to understand. While profiling my code it came to
    light that about 80% of the algorithm was spent creating all the
    AllocationsModel instances. Querying only the fields that we need and
    by that not creating the model instances makes it much faster. As we
    don't need the instances anyway with the rewrite (we're doing most of
    the aggregation via set operations) changing this works quite well.
    
    The code now actually also has tests that it does what it should do.
    
    In the long run we want to remove the custom allocation management from
    the driver, which also should be possible as segment allocation in
    Neutron is less (not?) prone to race conditions anymore since Yoga.
    Transitioning without interrupting the ability to create new segments
    and without introducing segmentation inconsistencies is not trivial
    though, which is why we go with the sync rewrite before developing the
    whole migration.

---
+++ networking_aci/tests/unit/plugins/ml2/drivers/mech_aci/test_allocations_manager.py
@@ -0,0 +1,99 @@
+# Copyright 2024 SAP SE
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from unittest import mock
+
+from neutron_lib import context
+from neutron_lib.db import api as db_api
+
+from neutron.tests.unit.db.test_db_base_plugin_v2 import NeutronDbPluginV2TestCase
+
+
+from networking_aci.db.models import AllocationsModel
+from networking_aci.plugins.ml2.drivers.mech_aci.allocations_manager import AllocationsManager
+from networking_aci.plugins.ml2.drivers.mech_aci import common
+from networking_aci.plugins.ml2.drivers.mech_aci.config import ACI_CONFIG
+
+
+class TestAllocationsManager(NeutronDbPluginV2TestCase):
+    def test_sync_allocations(self):
+        ctx = context.get_admin_context()
+        net1 = self._make_network("json", "net1", True)['network']
+        net2 = self._make_network("json", "net2", True)['network']
+        net3 = self._make_network("json", "net3", True)['network']
+
+        start_data = [
+            ('seagull', 42, None),  # keep
+            ('seagull', 43, net1['id']),  # keep
+            ('seagull', 2323, None),  # delete (out of range)
+            ('seagull', 2324, net2['id']),  # keep (out of range, has network)
+            ('crow', 2323, None),  # delete (unknown hg)
+            ('crow', 2324, net3['id']),  # keep (unknown hg, has network)
+        ]
+
+        expected_result = [
+            ('crow', 2324, net3['id']),
+            ('oystercatcher', 100, None),
+            ('oystercatcher', 101, None),
+            ('oystercatcher', 102, None),
+            ('oystercatcher', 103, None),
+            ('seagull', 42, None),
+            ('seagull', 43, net1['id']),
+            ('seagull', 44, None),
+            ('seagull', 45, None),
+            ('seagull', 2324, net2['id']),
+
+            ('sentinel_1', 1, None),
+            ('sentinel_2', 2, None),
+        ]
+
+        with db_api.CONTEXT_WRITER.using(ctx) as sess:
+            for host, seg_id, net_id in start_data:
+                sess.add(AllocationsModel(host=host, level=1, segment_type="vlan", segmentation_id=seg_id,
+                                          network_id=net_id))
+            sess.add(AllocationsModel(host="sentinel_1", level=2, segment_type="vlan", segmentation_id=1))
+            sess.add(AllocationsModel(host="sentinel_2", level=1, segment_type="foo", segmentation_id=2))
+
+        db = common.DBPlugin()
+        ACI_CONFIG.db = db
+
+        hostgroups = {
+            'seagull': {
+                'direct_mode': False,
+                'hosts': [],
+                'segment_range': [(42, 45)],
+                'segment_type': 'vlan',
+            },
+            'oystercatcher': {
+                'direct_mode': False,
+                'hosts': [],
+                'segment_range': [(100, 103)],
+                'segment_type': 'vlan',
+            },
+            'sparrow': {
+                'direct_mode': True,
+                'hosts': [],
+                'segment_range': [(123, 234)],
+                'segment_type': 'vlan',
+            },
+        }
+
+        with mock.patch('networking_aci.plugins.ml2.drivers.mech_aci.config.ACIConfig.hostgroups',
+                        new_callable=mock.PropertyMock, return_value=hostgroups):
+            AllocationsManager(db)
+
+        with db_api.CONTEXT_READER.using(ctx) as sess:
+            db_result = [(a.host, a.segmentation_id, a.network_id) for a in sess.query(AllocationsModel).all()]
+
+        self.assertEqual(sorted(expected_result), sorted(db_result))
